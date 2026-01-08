23294c7b4b5833b27df1f746bad23c871a35d3dd
Subhdeep <subhdeeppal7@gmail.com>
2026-01-08 15:16:44 +0530

    transferfrom-test-done

--- test/MyToken.t.sol
+++ test/MyToken.t.sol
@@ -25,6 +25,25 @@ contract MyTokenTest is Test {
     function testTransfer() public {
         m.mint(address(this) , 100) ;
         m.transfer(0xeD5b4Dc4DA48c56E6983A584c98cBE47C123f1B9 , 50);
+        
         assertEq(m.balanceOf(0xeD5b4Dc4DA48c56E6983A584c98cBE47C123f1B9), 50 , "ok");
+        assertEq(m.balanceOf(address(this)) , 50 , "ok");
+
+        vm.prank(0xeD5b4Dc4DA48c56E6983A584c98cBE47C123f1B9);
+        m.transfer(address(this) , 50 ) ;
+
+        assertEq(m.balanceOf(address(this)), 100 , "ok");
+        assertEq(m.balanceOf(0xeD5b4Dc4DA48c56E6983A584c98cBE47C123f1B9), 0 , "ok");
+    }
+
+    function testApprovals() public {
+        m.mint(address(this) , 100 ) ;
+        m.approve(0xeD5b4Dc4DA48c56E6983A584c98cBE47C123f1B9 , 50) ;
+
+        vm.prank(0xeD5b4Dc4DA48c56E6983A584c98cBE47C123f1B9); 
+        m.transferFrom(address(this) ,0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 , 40 ) ;
+
+        assertEq(m.balanceOf(address(this)), 60 , "ok" );
+        assertEq(m.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4) , 40 , "ok");
     }
 }
