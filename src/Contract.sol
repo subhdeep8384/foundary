245dc100e0b1581a3cfeb25e7ea439b9f85cb42c
Subhdeep <subhdeeppal7@gmail.com>
2026-01-08 16:25:17 +0530

    added

--- src/Contract.sol
+++ src/Contract.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Contract { 
+    string public value ;
     string public name ;
     uint public number ;
     constructor(string memory _name , uint _number) {
